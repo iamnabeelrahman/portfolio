f959bcbb8afbd32c0496b54291217709f87aa566
Nabeel Rahman <122614622+iamnabeelrahman@users.noreply.github.com>
2025-01-03 05:07:20 +0530

    Update project-data.tsx

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -11,7 +11,7 @@ export const projects: Project[] = [
     year: 2023,
     description:
       "This portfolio project showcases my skills as a full-stack developer, featuring a clean and modern design with an emphasis on functionality and user experience. Built using Next.js, the website is responsive and optimized for both mobile and desktop views.",
-    url: "https://example.com/",
+    url: "https://portfolio-git-main-nabeel-rahmans-projects.vercel.app/",
   },
 
 ];
